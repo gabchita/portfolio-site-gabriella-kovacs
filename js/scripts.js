b3e4ae5389d3cdc8c8935f45925605f3a5de9efc
Gabriella Kovacs <gabchita@gmail.com>
2022-07-19 16:24:27 +0100

    Fixed header issue

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 console.log("Hi! Welcome to my Portfolio Site");
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
-  if (x.className === 'navtoggle') {
+  if (x.className === 'navtoggle' && $(window).width() < 640) {
     x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
